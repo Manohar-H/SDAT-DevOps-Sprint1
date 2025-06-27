82d7dbb1e1843051c64fc8a70e2ff209b7d313d0
Harini Manohar <harini.manohar2@gmail.com>
2025-06-27 11:58:47 -0230

    Initial Spring Boot setup with base app and config

---
+++ src/main/java/com/example/sdat/SdatApplication.java
@@ -0,0 +1,11 @@
+package com.example.sdat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SdatApplication.class, args);
+    }
+}
